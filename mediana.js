ae190f14eb8fa9d3bec9c78eab8a48dc2ac6336d
Mart <marta.nikiel@yahoo.com>
2024-08-06 13:01:04 +0200

    ready

--- mediana.js
+++ mediana.js
@@ -1,4 +1,4 @@
-const expenses = {
+let expenses = {
   "2023-01": {
     "01": {
       food: [22.11, 43, 11.72, 2.2, 36.29, 2.5, 19],
@@ -20,83 +20,133 @@ const expenses = {
   },
   "2023-04": {},
 };
+function addExp() {
+  const obj = {};
 
-function getMedianUntilFirstSunday(expenses) {
-  const firstWeekExpenses = {};
-  const medianFirstWeek = {};
+  const date = document.getElementById("date").value;
+  const category = document.getElementById("category").value.toLowerCase();
+  const exp = document.getElementById("exp").value;
+  const yearMonth = date.substring(0, 7);
+  const day = date.toString().substring(8, 10);
 
+  obj[yearMonth] = {
+    [day]: {
+      [category]: [Number(exp)],
+    },
+  };
+
+  const check = [];
+
+  for (const yM in expenses) {
+    if (yM == yearMonth) {
+      check.push("y-m");
+    }
+    for (const d in expenses[yM]) {
+      if ((yM == yearMonth) & (d == day)) {
+        check.push("y-m, d");
+      }
+      for (const cat in expenses[yM][d]) {
+        if ((yM == yearMonth) & (d == day) & (cat == category)) {
+          check.push("y-m, d, cat");
+        }
+      }
+    }
+  }
+  if (check.length == 3) {
+    expenses[yearMonth][day][category].push(Number(exp));
+  } else if (check.length == 2) {
+    expenses[yearMonth][day] = Object.assign(
+      expenses[yearMonth][day],
+      obj[yearMonth][day]
+    );
+  } else if (check.length == 1) {
+    expenses[yearMonth] = Object.assign(expenses[yearMonth], obj[yearMonth]);
+  } else {
+    expenses = Object.assign(expenses, obj);
+  }
+
+  //console.log(check.length);
+  console.log(expenses);
+}
+
+const firstWeekMonthExpenses = {};
+const medianFirstWeekMonth = {};
+
+function get_median_of_first_week_expenses() {
   for (const month in expenses) {
     if (Object.keys(expenses[month]).length === 0) {
-      firstWeekExpenses[`${month}`] = `Brak danych!`;
+      firstWeekMonthExpenses[`${month}`] = `Brak danych!`;
     }
     for (const day in expenses[month]) {
       const dayStat = day;
       const date = `${month}-${day}`;
       const dateConvert = new Date(`${month}-${day}`);
-      const dayOfWeek = dateConvert.getDate();
+      const dayOfWeek = dateConvert.getDay();
       const monthOfYear = dateConvert.getMonth();
-
       if (dayOfWeek === 0 && day <= 7) {
-        firstWeekExpenses[`${month}`] = `${Object.values(
+        firstWeekMonthExpenses[`${month}`] = `${Object.values(
           expenses[month][day]
         )}`;
       } else if (dayOfWeek === 1 && day == 1) {
-        firstWeekExpenses[`${month}`] = `${Object.values(
+        firstWeekMonthExpenses[`${month}`] = `${Object.values(
           expenses[month][day]
         )}`;
       } else if (dayOfWeek === 2 && day <= 2) {
-        firstWeekExpenses[`${month}`] = `${Object.values(
+        firstWeekMonthExpenses[`${month}`] = `${Object.values(
           expenses[month][day]
         )}`;
       } else if (dayOfWeek === 3 && day <= 3) {
-        firstWeekExpenses[`${month}`] = `${Object.values(
+        firstWeekMonthExpenses[`${month}`] = `${Object.values(
           expenses[month][day]
         )}`;
       } else if (dayOfWeek === 4 && day <= 4) {
-        firstWeekExpenses[`${month}`] = `${Object.values(
+        firstWeekMonthExpenses[`${month}`] = `${Object.values(
           expenses[month][day]
         )}`;
       } else if (dayOfWeek === 5 && day <= 5) {
-        firstWeekExpenses[`${month}`] = `${Object.values(
+        firstWeekMonthExpenses[`${month}`] = `${Object.values(
           expenses[month][day]
         )}`;
       } else if (dayOfWeek === 6 && day <= 6) {
-        firstWeekExpenses[`${month}`] = `${Object.values(
+        firstWeekMonthExpenses[`${month}`] = `${Object.values(
           expenses[month][day]
         )}`;
       } else {
-        firstWeekExpenses[`${date}`] = `Dzień poza zakresem analizy!`;
+        firstWeekMonthExpenses[`${date}`] = `Dzień poza zakresem analizy!`;
       }
     }
   }
-
-  for (const exp in firstWeekExpenses) {
+  for (const exp in firstWeekMonthExpenses) {
     if (
-      firstWeekExpenses[exp] !== "Dzień poza zakresem analizy!" &&
-      firstWeekExpenses[exp] !== "Brak danych!"
+      firstWeekMonthExpenses[exp] !== "Dzień poza zakresem analizy!" &&
+      firstWeekMonthExpenses[exp] !== "Brak danych!"
     ) {
-      const split = firstWeekExpenses[exp]
+      const split = firstWeekMonthExpenses[exp]
         .split(",")
         .filter(Boolean)
         .slice()
         .sort((a, b) => a - b);
       const mid = Math.floor(split.length / 2);
-      firstWeekExpenses[exp] = split;
-
+      firstWeekMonthExpenses[exp] = split;
       if (split.length % 2 === 0) {
-        medianFirstWeek[`${exp}`] = (
+        medianFirstWeekMonth[`${exp}`] = (
           (Number(split[mid - 1]) + Number(split[mid])) /
           2
         ).toString();
       } else {
-        medianFirstWeek[`${exp}`] = split[mid];
+        medianFirstWeekMonth[`${exp}`] = split[mid];
       }
-    } else if (firstWeekExpenses[exp] === "Brak danych!") {
-      medianFirstWeek[`${exp}`] = "Brak danych";
+    } else if (firstWeekMonthExpenses[exp] === "Brak danych!") {
+      medianFirstWeekMonth[`${exp}`] = "Brak danych!";
     }
   }
-  console.log(firstWeekExpenses);
-  console.log(medianFirstWeek);
+
+  for (const date in medianFirstWeekMonth) {
+    const para2 = document.createElement("p");
+    para2.textContent = `${date} = ${medianFirstWeekMonth[date]}`;
+    document.body.appendChild(para2);
+  }
 }
 
-getMedianUntilFirstSunday(expenses);
+const btn = document.getElementById("calc");
+btn.addEventListener("click", get_median_of_first_week_expenses);
